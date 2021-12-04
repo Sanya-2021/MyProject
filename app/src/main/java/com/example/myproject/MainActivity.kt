8fbc0c77570db5e92919dd10945c8e0b0c76fa27
Sanya-2021 <95037493+Sanya-2021@users.noreply.github.com>
2021-12-04 14:55:02 +0300

    v.1

---
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+
+    fun toastME(view: android.view.View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+}
